8e9e1c6850eb3c9b444f6c5a95a1d2fb7924cff5
Игорь <211353@mail.ru>
2023-02-06 18:37:10 +0300

    finish

---
+++ task1.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+
+вводим через один Enter (разбиваем через Split())
+
+*/
+
+string getStringNumber()
+{
+    return Console.ReadLine();
+}
+int[] numberSplit(string number)
+{
+    string[] myArray = number.Split(',');
+    int[] array = new int[myArray.Length];
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        array[i] = Convert.ToInt32(myArray[i]);
+    }
+
+    return array;
+}
+
+void printArray(int[] array)
+{
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+    Console.Write(array[array.Length - 1]);
+}
+
+int getPositiveNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+string lastNumber = getStringNumber();
+int[] arrayOffNumbers = numberSplit(lastNumber);
+printArray(arrayOffNumbers);
+Console.WriteLine($"-> {getPositiveNumber(arrayOffNumbers)}");
---
+++ task2.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+/*
+Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+double getNumber(){
+    return Convert.ToDouble(Console.ReadLine());
+}
+void findDot(){
+    Console.WriteLine("пожалуйста введите k1: ");
+    double k1 = getNumber();
+    Console.WriteLine("пожалуйста введите k2: ");
+    double k2 = getNumber();
+    Console.WriteLine("пожалуйста введите b1: ");
+    double b1 = getNumber();
+    Console.WriteLine("пожалуйста введите b2: ");
+    double b2 = getNumber();
+    double result1 = (b2 - b1)/(k1 - k2);
+    double result2 = k1 * result1 + b1;
+    Console.WriteLine($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({result1}; {result2})");
+}
+
+findDot();
